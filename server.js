34abc4548077c0baea9b7a929f996c20999891a9
ruzanna07 <xachatryanruzanna5>
2022-10-17 22:05:40 +0400

    development and production

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const config = require('./config');
+
+const app = express();
+
+console.log(`NODE_ENV=${config.NODE_ENV}`);
+
+app.get('/',(req,res) => {
+    res.send('Hello')
+})
+
+app.listen(config.PORT,config.HOST,()=>{
+    console.log(`APP LISTENING ON http://${config.HOST}:${config.PORT}`)
+})
